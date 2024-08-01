480cc511ca86cce573c1142ac18f74ac51b3ce7a
KOlguin <kolguin@uc.cl>
2024-08-01 12:58:03 -0400

    Se agrega ruta para obtener todos los directores

--- Backend/routes/OwenWilsonAPI.js
+++ Backend/routes/OwenWilsonAPI.js
@@ -15,4 +15,16 @@ router.get("/all-movies", async(req, res) => {
     }
 })
 
+router.get("/all-directors", async(req, res) => {
+    try {
+        const {data: directors} = await axios.get("https://owen-wilson-wow-api.onrender.com/wows/directors")
+
+        res.json(directors);
+    }
+    catch (error) {
+        console.log("Error al obtener todos los directores, ", error);
+        res.status(500).json({error: "Error interno del servidor"});
+    }
+})
+
 module.exports = router;
