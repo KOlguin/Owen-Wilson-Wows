28579fe8a6d846f24151fe730a727257c3ecf103
KOlguin <kolguin@uc.cl>
2024-08-04 13:22:20 -0400

    Se agrega información base de Landing Page

---
+++ Frontend/src/Components/navbar.jsx
@@ -0,0 +1,53 @@
+// import React from 'react'
+
+// const Navbar = () => {
+//     return (
+//         <div>Navbar</div>
+//     )
+// }
+
+// export default Navbar
+
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import logo from '../assets/Image/Logo.jpg'
+
+function NavbarFunction() {
+  return (
+    <>
+        <style type="text/css">
+        {`
+            .navbar { background-color: #F6F6F6}
+            .nav-link { color: #444444; font-weight: bold; font-family: Helvetica;}
+
+        `}
+            {/* Agrandar font de nav link  */}
+        </style>
+
+        <Navbar sticky="top">
+            <Container>
+                <Navbar.Brand href="/">
+                    <img
+                        src={logo}
+                        width="130px"
+                        height="80px"
+                        className="d-inline-block align-top"
+                        alt="React Bootstrap logo"
+                    />
+                </Navbar.Brand>
+                <Navbar.Toggle />
+                <Navbar.Collapse className="justify-content-end">
+                    <Nav>
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/">Categorias</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    </>
+    
+  );
+}
+
+export default NavbarFunction;
