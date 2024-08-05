8868563be85b92ca7f40fb549588e912dac7c192
KOlguin <kolguin@uc.cl>
2024-08-05 15:47:20 -0400

    Se arregla es uso de audio en wowsCategory.jsx

--- Frontend/src/Components/wowsCategory.jsx
+++ Frontend/src/Components/wowsCategory.jsx
@@ -1,3 +1,5 @@
+import { React, useState, useEffect } from 'react'
+
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -8,29 +10,33 @@ import '../assets/styles/wowsCategory.css'
 
 function WowsCategory(props) {
     // Obtenido de https://www.geeksforgeeks.org/how-to-toggle-play-pause-in-reactjs-with-audio/
-    var state = {
+    // var state = {
  
-        // Get audio file in a variable
-        audio: new Audio(props.audio),
+    //     // Get audio file in a variable
+    //     audio: new Audio(props.audio),
  
-        // Set initial state of song
-        isPlaying: false,
-    };
+    //     // Set initial state of song
+    //     isPlaying: false,
+    // };
+    var audio = new Audio(props.audio)
+    // const [isPlaying, setIsPlaying] = useState(false);
 
-    function PlayPause() {
-        let isPlaying = state.isPlaying;
+    function PlayAudio() {
+        // let isPlaying = state.isPlaying;
+        // setIsPlaying(false);
  
-        if (isPlaying) {
-            // Pause the song if it is playing
-            state.audio.pause();
-        } else {
- 
-            // Play the song if it is paused
-            state.audio.play();
-        }
- 
+        // if (isPlaying) {
+        //     // Pause the song if it is playing
+        //     audio.pause();
+        // } else {
+        //     setIsPlaying(true);
+        //     // Play the song if it is paused
+        //     audio.play();
+        // }
+        audio.play()
         // Change the state of song
-        setState({ isPlaying: !isPlaying });
+        // setState({ isPlaying: !isPlaying });
+        
     }
     
     return (
@@ -46,7 +52,7 @@ function WowsCategory(props) {
                     <p className='descriptionWow'><b>Linea en la que aparece el <i>wow</i></b>: {props.full_line}</p>
                     <p className='descriptionWow'><b>Momento en el cual se dice la línea anterior</b>: {props.timestamp}</p>
                     <p className='descriptionWow'><b>Audio con la línea anterior</b>:
-                        <Button variant="secondary" size="sm" onClick={PlayPause} style={{backgroundColor: "#275dad", borderColor:"#275dad"}}>
+                        <Button variant="secondary" size="sm" onClick={PlayAudio} style={{backgroundColor: "#275dad", borderColor:"#275dad"}}>
                             Play
                         </Button>
                     </p>
